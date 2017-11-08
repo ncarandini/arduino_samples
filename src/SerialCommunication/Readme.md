268ad9bc686320caf584de8f75a0c557534bc808
Nicolò Carandini <n.carandini@outlook.com>
2017-11-08 01:35:17 +0100

    Add readme and Serial Communication example Sketch

---
+++ src/SerialCommunication/Readme.md
@@ -0,0 +1,7 @@
+# Serial Communication Example
+
+Arduino Uno board has a USB port used to power it and to connect it to our PC installed Arduino IDE.
+
+With the Arduino IDE Console Window (activated by Ctrl + Shift + M) we can establish a bidirectional communication to and from the board.
+
+This small Arduino Sketch shows how to receive and send text from and to the Console.
---
+++ src/SerialCommunication/SerialCommunication.ino
@@ -0,0 +1,60 @@
+/*
+  Copyright (c) 2017 Nicolò Carandini. All right reserved. 
+
+  This file is licensed to you under the MIT license.
+  See the LICENSE file in the project root for more information.
+
+  This library use the code from http://forum.arduino.cc/index.php?topic=396450
+*/
+
+// Receive text with an end-marker (tipically a \n (newline)from the Console and echo it to the same Console.
+#include <Wire.h>
+
+const byte numChars = 64;
+char msg[numChars];   // an array to store the received data
+boolean newData = false;
+
+void setup() {
+   // set serial device
+  Serial.begin(9600);
+  Serial.println("Ready to receive your message...");
+}
+
+void loop() {
+  recvWithEndMarker();
+  showMsg();
+}
+
+void recvWithEndMarker() {
+  static byte ndx = 0;
+  char endMarker = '\n';
+  char rc;
+
+  while (Serial.available() > 0 && newData == false) {
+    rc = Serial.read();
+
+    if (rc != endMarker) {
+      msg[ndx] = rc;
+      ndx++;
+      if (ndx >= numChars) {
+        ndx = numChars - 1;
+      }
+    }
+    else {
+      msg[ndx] = '\0'; // terminate the string
+      ndx = 0;
+      newData = true;
+    }
+  }
+}
+
+void showMsg() {
+  if (newData == true) {
+    // Print back to the serial console
+    Serial.print("This is what I've received: ");
+    Serial.println(msg);
+    
+    // reset flag
+    newData = false;
+  }
+}
